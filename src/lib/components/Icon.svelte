1c561c1e1706266a0e3bcd784e5e7ad9486fac7c
Pinxuan Huang <pinxhuang@gmail.com>
2023-05-11 13:03:03 -0400

    Add background and folder images

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -4,7 +4,30 @@
 </script>
 
 <div>
-	<button on:dblclick={() => handleWindowOpen(win)}>{win.icon.name}</button>
+	<button on:dblclick={() => handleWindowOpen(win)} class="icon">
+		<img src={win.icon.path} alt={win.icon.name} />
+		{win.icon.name}
+	</button>
 </div>
 
-<style></style>
+<style>
+	.icon {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		font-weight: bold;
+		text-transform: uppercase;
+		letter-spacing: 1px;
+		font-size: 12px;
+		color: white;
+
+		padding: 8px;
+		margin: 6px;
+		background-color: transparent;
+		border: none;
+	}
+
+	.icon:hover {
+		cursor: pointer;
+	}
+</style>
--- src/lib/stores/windows.js
+++ src/lib/stores/windows.js
@@ -3,13 +3,13 @@ import { writable } from 'svelte/store';
 const window1 = {
     id: 1,
     title: "window 1",
-    icon: { name: "icon 1" },
+    icon: { name: "icon 1", path: "images/pastel-folder-64.png" },
     content: "window 1 content"
 }
 const window2 = {
     id: 2,
     title: "window 2",
-    icon: { name: "icon 2" },
+    icon: { name: "icon 2", path: "images/pastel-folder-64.png" },
     content: "window 2 content"
 }
 
